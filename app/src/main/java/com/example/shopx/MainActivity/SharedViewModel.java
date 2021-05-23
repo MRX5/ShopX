7be9a55d396a6347bd3470deca86414efc4cd699
Mostafa Gad <m.gad192@gmail.com>
2021-05-24 01:17:40 +0200

    commit

---
+++ app/src/main/java/com/example/shopx/MainActivity/SharedViewModel.java
@@ -0,0 +1,30 @@
+package com.example.shopx.MainActivity;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+
+import com.example.shopx.Model.Mobile;
+import com.example.shopx.Repository;
+
+import java.util.List;
+
+public class SharedViewModel extends ViewModel {
+
+    public MutableLiveData<List<Mobile>> mobiles=null;
+
+    public void sendMobiles(List<Mobile>results)
+    {
+        mobiles=new MutableLiveData<>();
+        mobiles.setValue(results);
+    }
+    public void cleanMemory()
+    {
+        mobiles=null;
+    }
+
+}
